8d0e65073a34433e8684f247cef14a213730aec6
Naphorn <naphorn.chok@gmail.com>
2017-11-04 22:02:08 +0700

    Test cheked gpa > 4.0

--- Part01/Part01/Program.cs
+++ Part01/Part01/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp5
             try
             {
                 // su.Name = "My name";
-                su.GPA = 3.5f;
+                su.GPA = 4.5f;
                 Console.WriteLine("Student name : " + su.Name);
                 Console.WriteLine("Student ID   : " + su.ID);
                 Console.WriteLine("Student GPA  : " + su.GPA);
