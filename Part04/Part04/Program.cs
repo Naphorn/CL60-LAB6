fdea654436497c8ee6cc8d7d179401c5f153275a
Naphorn <naphorn.chok@gmail.com>
2017-11-10 00:31:38 +0700

    Test 1

---
+++ Part04/Part04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Part04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Professor();
+            p.SayHi();
+            Console.ReadKey();
+        }
+
+
+    }
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+
+    class Professor : Teacher
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a Professor");
+        }
+    }
+}
