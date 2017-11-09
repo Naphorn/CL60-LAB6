d2ce7db65e60db02e493f36e49836af5f56bca9c
Naphorn <naphorn.chok@gmail.com>
2017-11-10 00:20:18 +0700

    Dynamic polymorphism

---
+++ Part03.2/Part03.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Part03._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+
+}
